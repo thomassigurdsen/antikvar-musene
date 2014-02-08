4cd58b5fb6cb3355fec3dd835bf91093d5ecf328
Thomas Sigurdsen <thomas.sigurdsen@gmail.com>
2014-02-08 03:14:58 +0100

    Add working hardcoded api call to unimus

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 function xhr() {
-	xhr = new XMLHttpRequest();
-	xhr.open('GET', 'http://www.unimus.no/artefacts/khm/51981/', false);
-	xhr.addEventListener('load', function() {
-		console.log(xhr.responseText);
-		return xhr.responseText;
-	});
-	xhr.send();
+	var xhrob = new XMLHttpRequest();
+	xhrob.open('GET', "http://www.unimus.no/artefacts/khm/52981/", true);
+	xhrob.onreadystatechange = function (event) {
+		var xhttpr = event.target;
+		if (xhttpr.readyState === 4 && xhttpr.status === 200) {
+			document.getElementById("jsonpar").innerHTML = xhttpr.responseText;
+		}
+	};
+	xhrob.send();
 }
 
 function run() {
